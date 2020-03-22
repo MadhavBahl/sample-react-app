adc2250069d6411b726922915bcf8ac99171266c
MadhavBahlMD <madhavbahl10@gmail.com>
2020-03-22 20:33:29 +0530

    Updates

---
+++ precache-manifest.6537fa7805a76867fe82c8af050baf8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27079ecdbcf8dc8b114500ff34877d28",
+    "url": "/sample-react-app/index.html"
+  },
+  {
+    "revision": "7fec76744148b01875b6",
+    "url": "/sample-react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6dd5ca27bb196ec7ed19",
+    "url": "/sample-react-app/static/js/2.9847d4e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample-react-app/static/js/2.9847d4e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fec76744148b01875b6",
+    "url": "/sample-react-app/static/js/main.cbb9274d.chunk.js"
+  },
+  {
+    "revision": "c9f370ca44cc00e76feb",
+    "url": "/sample-react-app/static/js/runtime-main.1a33320b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
